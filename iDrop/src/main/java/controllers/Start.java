ad73054382a278fe3fb4358aa089397f340029ee
pure1017 <910709733@qq.com>
2020-11-12 11:24:45 +0800

    Add frontend files

---
+++ iDrop/src/main/java/controllers/Start.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import java.io.IOException;
+import java.util.Queue;
+import org.eclipse.jetty.websocket.api.Session;
+
+class Start {
+
+  private static final int PORT_NUMBER = 8080;
+
+  private static Javalin app;
+
+  /** Main method of the application.
+   * @param args Command line arguments
+   */
+
+  public static void main(final String[] args) {
+
+    app = Javalin.create(config -> {
+      config.addStaticFiles("/public");
+    }).start(PORT_NUMBER);
+
+    // Test Echo Server
+    app.post("/echo", ctx -> {
+      ctx.result(ctx.body());
+    });
+    
+    // New Game
+    app.get("/", ctx -> {
+      ctx.redirect("/index.html");
+    });
+  }
+  
+  /** Send message to all players.
+   * @param gameBoardJson Gameboard JSON
+   * @throws IOException Websocket message send IO Exception
+   */
+  private static void sendGameBoardToAllPlayers(final String gameBoardJson) {
+    Queue<Session> sessions = UiWebSocket.getSessions();
+    for (Session sessionPlayer : sessions) {
+      try {
+        sessionPlayer.getRemote().sendString(gameBoardJson);
+      } catch (IOException e) {
+        // Add logger here
+      }
+    }
+  }
+
+  public static void stop() {
+    app.stop();
+  }
+}
