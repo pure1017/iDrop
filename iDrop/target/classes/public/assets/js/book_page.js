e06f26a0a2bb91eb65026a4c7e6a420b10ea58da
pure1017 <910709733@qq.com>
2020-11-22 16:04:51 +0800

    handle application

--- iDrop/target/classes/public/assets/js/book_page.js
+++ iDrop/target/classes/public/assets/js/book_page.js
@@ -80,12 +80,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         let reader_comment = form.reader_comment.value
 
-        let param = '?userId='+userId+'&itemId='+itemId+'&rating='+reader_rate+'&comment='+reader_comment;
+        let param = '?userId='+userId+'&itemId='+itemId+'&rating='+reader_rate+'&comment='+reader_comment+'&time='+formatDateTime(Date.now());
+        console.log(formatDateTime(Date.now()));
         ajax('POST', '/rating'+param, req,
-                // successful callback
+            // successful callback
             function(res) {
                 if (res === "book rated") {
-                    console.log(res);
                     setTimeout(function () {
                         alert("rating submitted!");
                     }, 10);
@@ -101,6 +101,38 @@ document.addEventListener('DOMContentLoaded', function () {
                 showErrorMessage('Cannot rate items.');
             });
     });
+
+    //get user rating and comments
+    let req = JSON.stringify({});
+    let param = "?itemId="+itemId;
+    ajax('GET', '/getrating'+param, req,
+        // successful callback
+        function(res) {
+            let comment_HTML = ''
+            const div_comment = document.getElementById("comment_list");
+            div_comment.innerHTML = '';
+            if (res !== "input error") {
+                var items = JSON.parse(res);
+                for (let item in items) {
+                    var rating = items[item][1];
+                    var comment = items[item][2];
+                    var time = items[item][3];
+                    comment_HTML += '<hr style="filter: alpha(opacity=80,finishOpacity=30,style=1)" width="80%" color=lightgray size=3>\n' +
+                        '          <div style="clear: left;">\n' +
+                        '            <p style="float: left;"><img src="assets/img/user_icon.jpg" height="100px" width="100px" border="1px" style="margin-right: 20px"></p>\n' +
+                        '            <p>Rating: '+ time +'</p>\n' +
+                        '            <p>Rating: '+ rating +'</p>\n' +
+                        '            <p>Comment: '+ comment +'</p>\n' +
+                        '          </div>'
+                }
+            }
+            div_comment.innerHTML = comment_HTML;
+        },
+        // failed callback
+        function() {
+            showErrorMessage('Cannot rate items.');
+        });
+
 });
 
 /**
@@ -148,3 +180,20 @@ function ajax(method, url, data, callback, errorHandler) {
 }
 
 module.exports = ajax;
+
+function formatDateTime(timeStamp) {
+    var date = new Date();
+    date.setTime(timeStamp);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;
+}
