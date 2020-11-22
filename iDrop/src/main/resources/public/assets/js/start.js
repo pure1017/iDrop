52db09bc4d911cce76b3485aefe467c47f1339dd
pure1017 <910709733@qq.com>
2020-11-23 01:09:34 +0800

    recommend part

--- iDrop/src/main/resources/public/assets/js/start.js
+++ iDrop/src/main/resources/public/assets/js/start.js
@@ -55,18 +55,35 @@ document.addEventListener('DOMContentLoaded', function () {
                 sessionStorage.setItem("author_recommend_tmp", JSON.stringify(author_recommend_tmp));
                 sessionStorage.setItem("cover_recommend_tmp", JSON.stringify(cover_recommend_tmp));
                 sessionStorage.setItem("desc_recommend_tmp", JSON.stringify(desc_recommend_tmp));
+                console.log(bookName_recommend_tmp);
 
+                const div_recommend_list = document.getElementById("recommend_list_container");
+                var recommend_innerHTML = '';
                 for (let i = 0; i < bookName_recommend_tmp.length; i++) {
-                    var div_recommend = document.getElementById("recommend"+(1+i));
-                    div_recommend.innerHTML = "";
-                    div_recommend.innerHTML = '<img src='+ cover_recommend_tmp[i] +' class="testimonial-img" alt="">\n' +
+                    // let div_recommend = document.getElementById("recommend3");
+                    // console.log(i);
+                    // div_recommend.innerHTML = "";
+                    // div_recommend.innerHTML = '<img src=' + cover_recommend_tmp[2] + ' class="testimonial-img" alt="">\n' +
+                    //     '              <h3>' + bookName_recommend_tmp[2] + '</h3>\n' +
+                    //     '              <h4>' + author_recommend_tmp[2] + '</h4>\n' +
+                    //     '              <p>\n' +
+                    //     '                <i class="bx bxs-quote-alt-left quote-icon-left"></i>\n' + desc_recommend_tmp[2] +
+                    //     '                <i class="bx bxs-quote-alt-right quote-icon-right"></i>\n' +
+                    //     '              </p>';
+                    recommend_innerHTML += '<div class="testimonial-wrap">\n' +
+                        '            <div id="recommend'+ i+1 +'" class="testimonial-item">\n' +
+                        '              <img src='+ cover_recommend_tmp[i] +' class="testimonial-img" alt="">\n' +
                         '              <h3>'+ bookName_recommend_tmp[i] +'</h3>\n' +
                         '              <h4>'+ author_recommend_tmp[i] +'</h4>\n' +
                         '              <p>\n' +
                         '                <i class="bx bxs-quote-alt-left quote-icon-left"></i>\n' + desc_recommend_tmp[i] +
                         '                <i class="bx bxs-quote-alt-right quote-icon-right"></i>\n' +
-                        '              </p>'
+                        '              </p>\n' +
+                        '            </div>\n' +
+                        '          </div>';
                 }
+                div_recommend_list.innerHTML = recommend_innerHTML;
+
             },
             // failed callback
             function() {
