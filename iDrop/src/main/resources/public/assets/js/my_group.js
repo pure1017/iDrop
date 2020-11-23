d199071e345df3d36724bdbd4857861e385387fd
Feiqiang-shen <fs2693@columbia.edu>
2020-11-22 22:11:47 -0500

    First demo

--- iDrop/src/main/resources/public/assets/js/my_group.js
+++ iDrop/src/main/resources/public/assets/js/my_group.js
@@ -2,7 +2,7 @@ const userId = sessionStorage.getItem("userId");
 
 document.addEventListener('DOMContentLoaded', function () {
     let req = JSON.stringify({});
-    let param = '?userId='+11111;
+    let param = '?userId='+userId;
     ajax('GET',
         '/getusergroup'+param,
         req,
@@ -82,75 +82,80 @@ document.addEventListener('DOMContentLoaded', function () {
     var application_HTML = '';
     const div_application_container = document.getElementById("application_container");
     div_application_container.innerHTML = '';
-    for (let i = 0; i < group_list.length; i++) {
-        application_HTML += '<div style="clear: left;">\n' +
-            '              <p style="float: left;"><img src="assets/img/application_form.jpg" height="200px" width="180px" border="1px" style="margin-right: 20px"></p>\n' +
-            '              <p>Group Name: '+ group_list[i] +'</p>\n' +
-            '              <p>Apply Message: '+ message_list[i] +'</p>\n' +
-            '              <div class="application_list_content"><button class="application_list_btn" id="p'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-check"></i></button></div>\n' +
-            '              <div class="application_list_content"><button class="application_list_btn" id="f'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-times"></i></button></div>\n' +
-            '            </div>'
+    if (group_list) {
+        for (let i = 0; i < group_list.length; i++) {
+            application_HTML += '<div style="clear: left;">\n' +
+                '              <p style="float: left;"><img src="assets/img/application_form.jpg" height="200px" width="180px" border="1px" style="margin-right: 20px"></p>\n' +
+                '              <p>Group Name: '+ group_list[i] +'</p>\n' +
+                '              <p>Apply Message: '+ message_list[i] +'</p>\n' +
+                '              <div class="application_list_content"><button class="application_list_btn" id="p'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-check"></i></button></div>\n' +
+                '              <div class="application_list_content"><button class="application_list_btn" id="f'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-times"></i></button></div>\n' +
+                '            </div>'
+        }
     }
+
     div_application_container.innerHTML = application_HTML;
+    if (group_list) {
+        for (let i = 0; i < group_list.length; i++) {
+            (function (){
+                var group_name = group_list[i];
+                var user_id = user_list[i];
+                var click_flag = false;
+                document.getElementById('p'+user_id+group_name).addEventListener('click', function (){
+                    if (click_flag === false) {
+                        click_flag = true;
+                        console.log("pass click");
+                        document.querySelector('#p' + user_id + group_name + ' i').style.color = '#ffc700';
+                        document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
+                        document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
+                        let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=true';
+                        let req = JSON.stringify({});
+                        ajax('POST', '/handleapplication'+param, req,
+                            function (res){
+                                if (res === "add success") {
+                                    setTimeout(function () {
+                                        alert("add member successfully!");
+                                    }, 10);
+                                } else {
+                                    setTimeout(function () {
+                                        alert(res);
+                                    }, 10);
+                                }
+                            },
+                            // failed callback
+                            function() {
+                                showErrorMessage('Cannot submit items.');
+                            });
+                    }
+                });
 
-    for (let i = 0; i < group_list.length; i++) {
-        (function (){
-            var group_name = group_list[i];
-            var user_id = user_list[i];
-            var click_flag = false;
-            document.getElementById('p'+user_id+group_name).addEventListener('click', function (){
-                if (click_flag === false) {
-                    click_flag = true;
-                    console.log("pass click");
-                    document.querySelector('#p' + user_id + group_name + ' i').style.color = '#ffc700';
-                    document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
-                    document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
-                    let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=true';
-                    let req = JSON.stringify({});
-                    ajax('POST', '/handleapplication'+param, req,
-                        function (res){
-                            if (res === "add success") {
-                                setTimeout(function () {
-                                    alert("add member successfully!");
-                                }, 10);
-                            } else {
-                                setTimeout(function () {
-                                    alert(res);
-                                }, 10);
-                            }
-                        },
-                        // failed callback
-                        function() {
-                            showErrorMessage('Cannot submit items.');
-                        });
-                }
-            });
-
-            document.getElementById('f'+user_id+group_name).addEventListener('click', function (){
-                if (click_flag === false) {
-                    click_flag = true;
-                    console.log("fail click");
-                    document.querySelector('#f' + user_id + group_name + ' i').style.color = '#ffc700';
-                    document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
-                    document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
-                    let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=false';
-                    let req = JSON.stringify({});
-                    ajax('POST', '/handleapplication'+param, req,
-                        function (res){
-                            if (res === "reject success") {
-                                setTimeout(function () {
-                                    alert("reject member successfully!");
-                                }, 10);
-                            }
-                        },
-                        // failed callback
-                        function() {
-                            showErrorMessage('Cannot submit items.');
-                        });
-                }
-            });
-        })();
+                document.getElementById('f'+user_id+group_name).addEventListener('click', function (){
+                    if (click_flag === false) {
+                        click_flag = true;
+                        console.log("fail click");
+                        document.querySelector('#f' + user_id + group_name + ' i').style.color = '#ffc700';
+                        document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
+                        document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
+                        let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=false';
+                        let req = JSON.stringify({});
+                        ajax('POST', '/handleapplication'+param, req,
+                            function (res){
+                                if (res === "reject success") {
+                                    setTimeout(function () {
+                                        alert("reject member successfully!");
+                                    }, 10);
+                                }
+                            },
+                            // failed callback
+                            function() {
+                                showErrorMessage('Cannot submit items.');
+                            });
+                    }
+                });
+            })();
+        }
     }
+
 });
 
 /**
