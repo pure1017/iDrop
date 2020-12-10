7676a25a43cde61a5a08802ca83c48746948c7fc
pure1017 <910709733@qq.com>
2020-12-10 12:51:45 +0800

    deal application

--- iDrop/src/main/resources/public/assets/js/my_group.js
+++ iDrop/src/main/resources/public/assets/js/my_group.js
@@ -84,10 +84,11 @@ document.addEventListener('DOMContentLoaded', function () {
         req,
         function (res) {
             var items = JSON.parse(res);
-
+            console.log(items);
             var group_list = [];
             var user_list = [];
             var message_list = [];
+            var status_list = [];
             for (let item in items) {
                 var group_name = Object.keys(items[item]);
                 for (let m in Object.keys(items[item][group_name])) {
@@ -95,15 +96,18 @@ document.addEventListener('DOMContentLoaded', function () {
                     if (items[item][group_name][message_name]["message"]) {
                         var message = items[item][group_name][message_name]["message"];
                         var user = items[item][group_name][message_name]["userId"];
+                        var status = items[item][group_name][message_name]["validm"];
                         group_list.push(group_name);
                         user_list.push(user);
                         message_list.push(message);
+                        status_list.push(status);
                     }
                 }
             }
             sessionStorage.setItem("group_list", JSON.stringify(group_list));
             sessionStorage.setItem("user_list", JSON.stringify(user_list));
             sessionStorage.setItem("message_list", JSON.stringify(message_list));
+            sessionStorage.setItem("status_list", JSON.stringify(status_list));
         },
         // failed callback
         function() {
@@ -115,80 +119,86 @@ document.addEventListener('DOMContentLoaded', function () {
     var user_list = JSON.parse(retrievedData2);
     var retrievedData3 = sessionStorage.getItem("message_list");
     var message_list = JSON.parse(retrievedData3);
+    var retrievedData4 = sessionStorage.getItem("status_list");
+    var status_list = JSON.parse(retrievedData4);
 
     var application_HTML = '';
     const div_application_container = document.getElementById("application_container");
     div_application_container.innerHTML = '';
     if (group_list) {
         for (let i = 0; i < group_list.length; i++) {
-            application_HTML += '<div style="clear: left;">\n' +
-                '              <p style="float: left;"><img src="assets/img/application_form.jpg" height="200px" width="180px" border="1px" style="margin-right: 20px"></p>\n' +
-                '              <p>Group Name: '+ group_list[i] +'</p>\n' +
-                '              <p>Apply Message: '+ message_list[i] +'</p>\n' +
-                '              <div class="application_list_content"><button class="application_list_btn" id="p'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-check"></i></button></div>\n' +
-                '              <div class="application_list_content"><button class="application_list_btn" id="f'+ user_list[i] + group_list[i] +'" type="submit"><i class="fa fa-times"></i></button></div>\n' +
-                '            </div>'
+            if (status_list[i] === "undecided") {
+                application_HTML += '<div style="clear: left;">\n' +
+                    '              <p style="float: left;"><img src="assets/img/application_form.jpg" height="200px" width="180px" border="1px" style="margin-right: 20px"></p>\n' +
+                    '              <p>Group Name: ' + group_list[i] + '</p>\n' +
+                    '              <p>Apply Message: ' + message_list[i] + '</p>\n' +
+                    '              <div class="application_list_content"><button class="application_list_btn" id="p' + user_list[i] + group_list[i] + '" type="submit"><i class="fa fa-check"></i></button></div>\n' +
+                    '              <div class="application_list_content"><button class="application_list_btn" id="f' + user_list[i] + group_list[i] + '" type="submit"><i class="fa fa-times"></i></button></div>\n' +
+                    '            </div>'
+            }
         }
     }
 
     div_application_container.innerHTML = application_HTML;
     if (group_list) {
         for (let i = 0; i < group_list.length; i++) {
-            (function (){
-                var group_name = group_list[i];
-                var user_id = user_list[i];
-                var click_flag = false;
-                document.getElementById('p'+user_id+group_name).addEventListener('click', function (){
-                    if (click_flag === false) {
-                        click_flag = true;
-                        console.log("pass click");
-                        document.querySelector('#p' + user_id + group_name + ' i').style.color = '#ffc700';
-                        document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
-                        document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
-                        let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=true';
-                        let req = JSON.stringify({});
-                        ajax('POST', '/handleapplication'+param, req,
-                            function (res){
-                                console.log(res)
-                                if (res === "add success") {
-                                    document.getElementById("modal_content").innerText = "add member successfully!";
-                                    $('#myModal').modal('show');
-                                } else {
-                                    document.getElementById("modal_content").innerText = res;
-                                    $('#myModal').modal('show');
-                                }
-                            },
-                            // failed callback
-                            function() {
-                                showErrorMessage('Cannot submit items.');
-                            });
-                    }
-                });
+            if (status_list[i] === "undecided") {
+                (function () {
+                    var group_name = group_list[i];
+                    var user_id = user_list[i];
+                    var click_flag = false;
+                    document.getElementById('p' + user_id + group_name).addEventListener('click', function () {
+                        if (click_flag === false) {
+                            click_flag = true;
+                            console.log("pass click");
+                            document.querySelector('#p' + user_id + group_name + ' i').style.color = '#ffc700';
+                            document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
+                            document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
+                            let param = '?hostId=' + userId + '&applicantId=' + user_id + '&groupName=' + group_name + '&add=true';
+                            let req = JSON.stringify({});
+                            ajax('POST', '/handleapplication' + param, req,
+                                function (res) {
+                                    console.log(res)
+                                    if (res === "add success") {
+                                        document.getElementById("modal_content").innerText = "add member successfully!";
+                                        $('#myModal').modal('show');
+                                    } else {
+                                        document.getElementById("modal_content").innerText = res;
+                                        $('#myModal').modal('show');
+                                    }
+                                },
+                                // failed callback
+                                function () {
+                                    showErrorMessage('Cannot submit items.');
+                                });
+                        }
+                    });
 
-                document.getElementById('f'+user_id+group_name).addEventListener('click', function (){
-                    if (click_flag === false) {
-                        click_flag = true;
-                        console.log("fail click");
-                        document.querySelector('#f' + user_id + group_name + ' i').style.color = '#ffc700';
-                        document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
-                        document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
-                        let param = '?hostId='+userId+'&applicantId='+user_id+'&groupName='+group_name+'&add=false';
-                        let req = JSON.stringify({});
-                        ajax('POST', '/handleapplication'+param, req,
-                            function (res){
-                                console.log(res)
-                                if (res === "reject success") {
-                                    document.getElementById("modal_content").innerText = "reject member successfully!";
-                                    $('#myModal').modal('show');
-                                }
-                            },
-                            // failed callback
-                            function() {
-                                showErrorMessage('Cannot submit items.');
-                            });
-                    }
-                });
-            })();
+                    document.getElementById('f' + user_id + group_name).addEventListener('click', function () {
+                        if (click_flag === false) {
+                            click_flag = true;
+                            console.log("fail click");
+                            document.querySelector('#f' + user_id + group_name + ' i').style.color = '#ffc700';
+                            document.getElementById("f" + user_id + group_name).style.cursor = 'auto';
+                            document.getElementById("p" + user_id + group_name).style.cursor = 'auto';
+                            let param = '?hostId=' + userId + '&applicantId=' + user_id + '&groupName=' + group_name + '&add=false';
+                            let req = JSON.stringify({});
+                            ajax('POST', '/handleapplication' + param, req,
+                                function (res) {
+                                    console.log(res)
+                                    if (res === "reject success") {
+                                        document.getElementById("modal_content").innerText = "reject member successfully!";
+                                        $('#myModal').modal('show');
+                                    }
+                                },
+                                // failed callback
+                                function () {
+                                    showErrorMessage('Cannot submit items.');
+                                });
+                        }
+                    });
+                })();
+            }
         }
     }
 
